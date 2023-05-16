a535b13bd6517026a57376890c96351ee456f472
LUJAIN ALKANHAL <lujain.i.k20@gmail.com>
2023-05-16 14:07:36 +0300

    Initial Commit

---
+++ DeliverooApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DeliverooApp
+//
+//  Created by لجين إبراهيم الكنهل on 26/10/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DeliverooApp/DeliverooAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliverooAppApp.swift
+//  DeliverooApp
+//
+//  Created by لجين إبراهيم الكنهل on 26/10/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct DeliverooAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
